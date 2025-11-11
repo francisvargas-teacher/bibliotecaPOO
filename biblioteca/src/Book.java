061455fa8fa278df878b18ce08e0dd1899c47422
Vinicius <vinicius.casagrande@rede.ulbra.br>
2025-11-11 16:32:10 -0300

    feat:media/book

--- biblioteca/src/Book.java
+++ biblioteca/src/Book.java
@@ -1,4 +1,26 @@
+package br.edu.ulbra.biblioteca;
+
+/**
+ * Representa um Livro, estendendo as funcionalidades básicas de MediaItem.
+ */
 public class Book extends MediaItem {
-    public String autor;
-    public String isbn;
+    private String author; // Corrigi o nome da variável para 'author'
+    private String isbn;
+
+    // CONSTRUTOR
+    // Deve chamar o construtor da superclasse (MediaItem) usando 'super()'
+    public Book(int id, String title, int year, int totalCopies, String author, String isbn) {
+        super(id, title, year, totalCopies); // Chama o construtor de MediaItem
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    // Getters para atributos específicos de Book
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
 }
--- biblioteca/src/MediaItem.java
+++ biblioteca/src/MediaItem.java
@@ -1,18 +1,65 @@
-import java.util.Date;
+package br.edu.ulbra.biblioteca;
 
+/**
+ * Classe base abstrata para todos os itens de mídia da biblioteca.
+ * Contém a lógica de controle de cópias.
+ */
 public abstract class MediaItem {
-    public String id;
-    public String title;
-    public int year;
-    public int totalCopies;
-    public int availableCopies;
+    // Usando 'protected' para permitir acesso direto pelas classes filhas (como Book)
+    protected int id;
+    protected String title;
+    protected int year;
+    protected int totalCopies;
+    protected int availableCopies;
 
-    public void borrow() {
-        System.out.println("Seu aluguél começou no dia " + Date);
+    // CONSTRUTOR: Essencial para inicializar todos os atributos
+    public MediaItem(int id, String title, int year, int totalCopies) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.totalCopies = totalCopies;
+        this.availableCopies = totalCopies; // Inicialmente, todas as cópias estão disponíveis
     }
-    public void returnItem () {
-        System.out.println("Você dever devolve-lo no dia " + Date);
 
+    /**
+     * Tenta emprestar uma cópia do item.
+     * @return true se o empréstimo for bem-sucedido, false caso contrário.
+     */
+    public boolean borrow() {
+        if (availableCopies > 0) {
+            availableCopies--;
+            System.out.println("SUCESSO: Uma cópia de '" + title + "' foi emprestada.");
+            System.out.println("Disponíveis restantes: " + availableCopies);
+            return true;
+        } else {
+            System.out.println("ERRO: Todas as " + totalCopies + " cópias de '" + title + "' estão emprestadas.");
+            return false;
+        }
     }
 
+    /**
+     * Recebe uma cópia do item de volta.
+     * @return true se a devolução for bem-sucedida, false caso contrário.
+     */
+    public boolean returnItem() {
+        if (availableCopies < totalCopies) {
+            availableCopies++;
+            System.out.println("SUCESSO: Uma cópia de '" + title + "' foi devolvida.");
+            System.out.println("Disponíveis agora: " + availableCopies);
+            return true;
+        } else {
+            // Isso só deve acontecer se houver um erro de lógica na aplicação
+            System.out.println("AVISO: A devolução não foi registrada. O total de cópias (" + totalCopies + ") já está no máximo.");
+            return false;
+        }
+    }
+
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
 }
